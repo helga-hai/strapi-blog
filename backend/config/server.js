ce792c7eaaec6560280d60f0b88256f56c54637e
Olha Hai-Nyzhnyk <olha@ribs.one>
2020-10-17 15:04:18 +0300

    first

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c6b0b751495f44beeb4e190824b2758'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '119c3e6f-9b3f-4021-91ff-3299e851f002'
+};
